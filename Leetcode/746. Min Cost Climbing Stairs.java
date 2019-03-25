f3d6beffb00aa259b034c4a155527c5d0a65a14c
Francis11h <zhan23@ncsu.edu>
2019-03-25 00:04:33 -0400

    同 70.climbStairs

---
+++ Leetcode/746. Min Cost Climbing Stairs.java
@@ -0,0 +1,70 @@
+746. Min Cost Climbing Stairs
+
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+Once you pay the cost, you can either climb one or two steps. 
+You need to find minimum cost to reach the top of the floor, 
+and you can either start from the step with index 0, or the step with index 1.
+
+可以从 第1级 或 第2级 开始走
+index:  0       1
+
+
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+
+
+dp[i] = cost[0];    i == 0
+        cost[1];    i == 1
+        Math.min(dp[i - 1], dp[i - 2]) + currentCost; i >= 2
+
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i <= n; i++) {
+            int currentCost = (i == n) ? 0 : cost[i];  //current step cost
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + currentCost;
+        }
+        return dp[n];
+    }
+}
+O(n)
+O(n)
+
+
+
+标答 递推
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int f1 = 0, f2 = 0;
+        for (int i = cost.length - 1; i >= 0; --i) {
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2 = f1;
+            f1 = f0;
+        }
+        return Math.min(f1, f2);
+    }
+}
+
+O(n)
+O(1)
+
+标答 另一种写法
+A bit DP and a bit Greedy
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
+    }
+}
