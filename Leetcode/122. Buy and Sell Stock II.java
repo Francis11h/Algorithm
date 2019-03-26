61ce82a10876be2718f2af7fa5ecfe3834d037d4
Francis11h <zhan23@ncsu.edu>
2019-03-25 20:32:23 -0400

    if (prices[i + 1] > prices[i])   maxProfit += prices[i + 1] - prices[i];

---
+++ Leetcode/122. Buy and Sell Stock II.java
@@ -0,0 +1,28 @@
+122. Best Time to Buy and Sell Stock II
+
+可多次买卖
+
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i])
+                maxProfit += prices[i + 1] - prices[i];
+        }
+        return maxProfit;
+    }
+}
