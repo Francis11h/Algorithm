ffcbe370aaca0fbbab292e6f0e4d722d11a3f2ce
Francis11h <zhan23@ncsu.edu>
2019-03-27 09:41:20 -0400

    adjacency list vs matrix

---
+++ tips/Graph.java
@@ -0,0 +1,15 @@
+图 基本 
+
+graph representation:
+
+adjacency matrix
+adjacency list
+edge list	(用于MST kruskal)
+
+ 
+								adjacency matrix		adjacency list
+inspect all edges					O(V^2)					O(E)
+Find all edges incident on i 		O(V)					O(k)
+Is there an edge from i to j		O(1)					O(logk)
+
+
