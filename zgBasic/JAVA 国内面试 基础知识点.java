765f2134e0957cdd9c0ff905aa711fbeae05040c
Francis11h <zhan23@ncsu.edu>
2019-03-30 21:44:11 -0400

    China

---
+++ zgBasic/JAVA 国内面试 基础知识点.java
@@ -0,0 +1,38 @@
+JAVA 国内面试 基础知识点
+
+什么是Java虚拟机？为什么Java被称作是“平台无关的编程语言”？
+static关键字是什么意思？Java中是否可以覆盖(override)一个private或者是static的方法？-baidu 1point3acres
+是否可以在static环境中访问非static变量？
+Java中的方法覆盖(Overriding)和方法重载(Overloading)是什么意思？
+Java中，什么是构造函数？什么是构造函数重载？什么是复制构造函数？
+Java支持多继承么？
+接口和抽象类的区别是什么？
+什么是值传递和引用传递？. check 1point3acres for more.
+java创建线程有几种不同的方式？你喜欢哪一种？为什么？
+概括的解释下线程的几种可用状态。
+同步方法和同步代码块的区别是什么？
+Java集合类框架的基本接口有哪些？
+为什么集合类没有实现Cloneable和Serializable接口？
+什么是迭代器(Iterator)？
+Java中的HashMap的工作原理是什么？ConcurrentHashMap呢？
+hashCode()和equals()方法的重要性体现在什么地方？
+数组(Array)和列表(ArrayList)有什么区别？什么时候应该使用Array而不是ArrayList？
+ArrayList和LinkedList有什么区别？
+java的动态代理机制是怎样的？
+
+如何权衡是使用无序的数组还是有序的数组？
+Enumeration接口和Iterator接口的区别有哪些？
+Java中垃圾回收有什么目的？什么时候进行垃圾回收？
+finalize()方法什么时候被调用？析构函数(finalization)的目的是什么？
+如果对象的引用被置为null，垃圾收集器是否会立即释放对象占用的内存？ 
+Java堆的结构是什么样子的？什么是堆中的永久代(Perm Gen space)?
+串行(serial)收集器和吞吐量(throughput)收集器的区别是什么？
+在Java中，对象什么时候可以被垃圾回收？
+JVM的永久代中会发生垃圾回收么？
+Java中的两种异常类型是什么？他们有什么区别？
+Java中Exception和Error有什么区别？
+throw和throws有什么区别？
+异常处理完成以后，Exception对象会发生什么变化？
+异常处理的时候，finally代码块的重要性是什么？
+finally代码块和finalize()方法有什么区别？
+java类是如何被虚拟机加载的？生命周期又是怎样的？
---
+++ zgBasic/spring面试题.java
@@ -0,0 +1,34 @@
+spring面试题
+
+什么是Spring框架？Spring框架有哪些主要模块？
+
+
+使用Spring框架有什么好处？
+什么是控制反转（IOC）？什么是依赖注入？
+
+
+请解释下Spring中的IOC？
+BeanFactory和ApplicationContext有什么区别？
+将Spring配置到你的应用中共有几种方法？
+
+什么基于XML的配置？
+什么基Java的配置？
+怎样用注解的方式配置Spring？
+描述Spring Bean的生命周期？
+描述Spring中各种Bean的范围？
+什么是Spring的嵌入beans？
+Spring框架中的单例bean是否是线程安全的？
+请举例说明如何用Spring注入一个Java的集合类？
+请举例说明如何在Spring的Bean中注入一个java.util.Properties？
+请解释Spring的Bean的自动生成原理？
+请辨析自动生成Bean之间模块的区别？
+
+如何开启基于基于注解的自动写入？
+    请举例说明@Required注解？
+    请举例说明@Autowired注解？
+    请举例说明@Qualifier注解？
+    
+请说明构造器注入和setter方法注入之间的区别？
+Spring框架中不同类型event有什么区别？
+FileSystemResource和ClassPathResource有何区别？
+请列举Spring框架中用了哪些设计模式？
