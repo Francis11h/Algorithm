f96268e4edf73c492dda381b5a0ac738ff6ac563
Francis11h <zhan23@ncsu.edu>
2019-04-03 13:02:46 -0400

    每次换的时候 用个 临时 Node 记录下 current节点原来next指向的 Node, 否则会丢失

---
+++ Leetcode/206. Reverse Linked List.java
@@ -0,0 +1,40 @@
+206. Reverse Linked List
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+
+         T
+    1 -> 2 -> 3 -> 4 -> 5 -> NULL
+p   c
+
+          T    
+ <- 1    2 -> 3 -> 4 -> 5 -> NULL
+    p    c
+
+              T    
+ <- 1 <- 2    3 -> 4 -> 5 -> NULL
+         p    c
+
+                             T    
+ <- 1 <- 2 <- 3 <- 4 <- 5  NULL
+                        p    c
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null, current = head;
+        while (current != null) {
+            ListNode Temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = Temp;
+        }
+        return prev;
+    }
+}
+
+每次换的时候 用个 临时 Node 记录下 current节点原来next指向的 Node, 否则会丢失
+
+O(n)
+O(1)
