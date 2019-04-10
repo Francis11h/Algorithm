a501d1b1b254ac673de98e5982db011aabcfe07d
Francis11h <zhan23@ncsu.edu>
2019-04-09 22:19:10 -0400

    先比A[mid],A[left]，if A[mid]大，就在xxxxoooo的x里面,因为 left 到 mid 是单增

--- Leetcode/033. Search in Rotated Sorted Array.java
+++ Leetcode/033. Search in Rotated Sorted Array.java
@@ -27,20 +27,29 @@ nums = [4,5,6,7,0,1,2]
 class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0)
-        	return -1;
+            return -1;
         int left = 0, right = nums.length - 1;
         while (left + 1 < right) {
-        	int mid = left + (right - left) / 2;
-        	if (nums[mid] == target)
-        		return mid;
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
 
-        	if (nums[left] < nums[mid]) {
-        		if (target > nums[left] && target < nums[mid])
-        			right = mid;
-        		else 
-        			left = mid;
-        	}
+            if (nums[left] < nums[mid]) {
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid;
+                else 
+                    left = mid;
+            } else {
+                if (target > nums[mid] && target <= nums[right]) 
+                    left = mid;
+                else 
+                    right = mid;
+            }
         }
-
+        if (nums[left] == target)
+            return left;
+        if (nums[right] == target)
+            return right;
+        return -1;
     }
 }
