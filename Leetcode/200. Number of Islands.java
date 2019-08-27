12783a0bdd657fe060f9e8a1c5dc8dedb8872f46
Francis11h <zhan23@ncsu.edu>
2019-08-27 15:59:51 -0400

    add dfs solution

--- Leetcode/200. Number of Islands.java
+++ Leetcode/200. Number of Islands.java
@@ -100,3 +100,42 @@ class Solution {
         }
     }
 }
+
+
+dfs beats 100% 
+
+visited[][] 可以去掉 但是 这样子 就改变了 原矩阵
+
+class Solution {
+    
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int ans = 0;
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(grid, i, j, visited);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    
+    private void dfs(char[][] grid, int x, int y, boolean[][] visited) {
+        int m = grid.length, n = grid[0].length;
+        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || grid[x][y] != '1') 
+            return;
+        
+        visited[x][y] = true;
+        dfs(grid, x + 1, y, visited);
+        dfs(grid, x - 1, y, visited);
+        dfs(grid, x, y + 1, visited);
+        dfs(grid, x, y - 1, visited);
+
+    }
+}
+
