af3ea5a8706f07133e552142c035066253f2f8ba
Francis11h <zhan23@ncsu.edu>
2019-09-09 19:34:15 -0400

    N-ary Tree Preorder Traversal 一样的 一个node的孩子从右到左入栈

--- Leetcode/144. Binary Tree Preorder Traversal.java
+++ Leetcode/144. Binary Tree Preorder Traversal.java
@@ -41,3 +41,60 @@ class Solution {
 }
 
 
+
+
+
+589. N-ary Tree Preorder Traversal
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;
+    }
+    private void helper(Node root, List<Integer> ans) {
+        if (root == null) return;
+        ans.add(root.val);
+        for (int i = 0; i < root.children.size(); i++) {
+            helper(root.children.get(i), ans);
+        }
+    }
+}
+
+
+
+从右到左的顺序往里加
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            ans.add(cur.val);
+            for (int i = cur.children.size() - 1; i >= 0; i--) {
+                stack.push (cur.children.get(i));
+            }
+        }
+        
+        return ans;
+    }
+}
+
