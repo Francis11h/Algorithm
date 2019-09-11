c87d394f240ae3aee23702c2bb490edf372f65a9
Francis11h <zhan23@ncsu.edu>
2019-09-10 20:20:39 -0400

    因为 必须是 root-to-leaf所以一边无leaf的时候不能算

--- Leetcode/111. Minimum Depth of Binary Tree.java
+++ Leetcode/111. Minimum Depth of Binary Tree.java
@@ -1,7 +1,7 @@
 111. Minimum Depth of Binary Tree
 
 The minimum depth is the number of nodes 
-along the shortest path from the root node down to the nearest leaf node.
+along the shortest path from the Root node down to the Nearest Leaf node.
 
 从 根节点 到 叶子结点 的最短路程
 
@@ -22,15 +22,35 @@ along the shortest path from the root node down to the nearest leaf node.
 
    =========> 3
 
+错误解法
+这么写 不符合本题题意 因为 要求 root-to-leaf 
+corner case : 
+    3
+   / 
+  9 
+这个 minDepth 应该是 2 不是 1, 因为 3 右边 无leaf 所以 不能算 root-to-leaf
 
+错误解法
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
+
+
+正确解法
 class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         int left = minDepth(root.left);
         int right = minDepth(root.right);
-        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+        return left == 0 || right == 0 ? left + right + 1 : Math.min(left, right) + 1;
     }
 }
+
+
+
 
 
 class Solution {
