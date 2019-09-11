8a357ba01c994f4687178ea1f3aa039e96c69e45
Francis11h <zhan23@ncsu.edu>
2019-09-11 10:10:52 -0400

    1

---
+++ Leetcode/L628.Maximum Subtree.java
@@ -0,0 +1,15 @@
+给你一棵二叉树，找二叉树中的一棵子树，他的所有节点之和最大
+返回这棵子树的根节点
+
+
+Input:
+{1,-5,2,0,3,-4,-5}
+Output:3
+Explanation:
+The tree is look like this:
+     1
+   /   \
+ -5     2
+ / \   /  \
+0   3 -4  -5
+The sum of subtree 3 (only one node) is the maximum. So we return 3.
