df9ed8df42c4d1253fafbf3a72b702534a337985
Francis11h <zhan23@ncsu.edu>
2019-09-10 20:27:20 -0400

    左子树平衡 + 右子树平衡 + |左右子树height差| <= 1

--- Leetcode/110. Balanced Binary Tree.java
+++ Leetcode/110. Balanced Binary Tree.java
@@ -8,14 +8,15 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
-        return isBalanced(root.left) && isBalanced(root.right) && (Math.abs(height(root.left) - height(root.right)) <= 1);
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(depth(root.left) - depth(root.right)) <= 1;
     }
     
-    private int height(TreeNode root) {
+    public int depth(TreeNode root) {
         if (root == null) return 0;
-        return Math.max(height(root.left), height(root.right)) + 1;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
     }
 }
+
 
 
 方法二 （并不直观）
