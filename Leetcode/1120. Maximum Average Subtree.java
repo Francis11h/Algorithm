d315fecb62438d586f313ad2cda54e86a4969c94
Francis11h <zhan23@ncsu.edu>
2019-09-11 10:37:01 -0400

    1

---
+++ Leetcode/1120. Maximum Average Subtree.java
@@ -0,0 +1,20 @@
+1120. Maximum Average Subtree
+
+Input：
+{1,-5,11,1,2,4,-2}
+Output：11
+Explanation:
+The tree is look like this:
+     1
+   /   \
+ -5     11
+ / \   /  \
+1   2 4    -2 
+The average of subtree of 11 is 4.3333, is the maximun.
+
+
+class Solution {
+    public double maximumAverageSubtree(TreeNode root) {
+        
+    }
+}
