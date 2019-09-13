dee9bc5989d1d786f335a231ea5eafb93fcfd793
Francis11h <zhan23@ncsu.edu>
2019-09-13 12:26:55 -0400

    O(MN 4^L)

--- Leetcode/079. Word Search.java
+++ Leetcode/079. Word Search.java
@@ -36,7 +36,7 @@ public class Solution {
     }
     // what param dfs need?  1.board 2.x 3.y 4.visited 5. index of the string 6. string
     // what dfs return? whether the char at board[x][y] == word.charAt(index)
-    //      if index == word.length ----> the word can be constructed from the board
+    //      if index == word.length() ----> the word can be constructed from the board
     private boolean dfs(char[][] board, int index, int x, int y, boolean[][] visited, String word) {
         int m = board.length, n = board[0].length;
         //found
@@ -56,76 +56,16 @@ public class Solution {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dfs 模仿bfs的写法  以后不要这么写！！ 
 DFS 还是要先写出口 然后 涉及方向的话 4个 排列 简单明了
 
 
-class Solution {
-    public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0 || board[0].length == 0) return false;
-        
-        int m = board.length, n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                visited[i][j] = true;       // 对这个 [["a","a"]] "aaa"  test case
-                if (dfs(board, 0, word, i, j, visited)) return true;
-                visited[i][j] = false;      // 对这个 [["a","a"]] "aaa"  test case
-            }
-        }
-        return false;
-    }
-                        
-    
-    private boolean dfs(char[][] board, int index, String word, int x, int y, boolean[][] visited) {
-        int m = board.length, n = board[0].length;
-        
-        int[] dirs = new int[] {1, 0, -1, 0, 1};
-        char ch = board[x][y];
-        if (ch != word.charAt(index)) {
-            return false;
-        }
-        if (index == word.length() - 1) {
-            return true;
-        }
-        
-        for (int i = 0; i < 4; i++) {
-            int adjx = x + dirs[i], adjy = y + dirs[i + 1];
-            if (adjx < 0 || adjx >= m || adjy < 0 || adjy >= n || visited[adjx][adjy]) continue;
-            visited[adjx][adjy] = true;
-            if (dfs(board, index + 1, word, adjx, adjy, visited)) {
-                return true;
-            }
-            visited[adjx][adjy] = false;
-        }
-        
-        return false;
-    }
-}
+time is O(M * N * 4^L) 
+where M*N is the size of the board and 
+we have 4^L for each cell because of the recursion.
+    if the average length is 5 , we will have 44444 total ways we could form a word on the board 
+            (note we ignore the boundaries of the board in those kind of calculations)
 
+S O(MN+L)
 
 
 
