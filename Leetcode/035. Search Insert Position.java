c771dd779505ccbb566c4872aaa265a1395b9aa9
Francis <42663441+Francis11h@users.noreply.github.com>
2019-10-18 22:58:21 -0400

    还是基本二分, 就是最后找不到的时候判断3种插入位置      [left, ,right] left  | right | right + 1

--- Leetcode/035. Search Insert Position.java
+++ Leetcode/035. Search Insert Position.java
@@ -15,6 +15,7 @@ Output: 1
     [left, ,right]
 left  | right | right + 1
 
+  第一次写的 笨方法
 class Solution {
     public int searchInsert(int[] nums, int target) {
         if (nums == null || nums.length == 0) return 0;
@@ -37,7 +38,28 @@ class Solution {
 }
 
 
-
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) { 
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        
+        if (nums[right] < target) return right + 1;
+        
+        if (nums[left] < target) return right;
+        
+        return left;
+    }
+}
 
 
 
