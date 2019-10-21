66c4c5979d209715d5887261065c59c59b6fb4e5
Francis11h <zhan23@ncsu.edu>
2019-10-21 14:45:54 -0400

    I calculated the stored water at each index a and b in my code.   At the start of every loop,      I update the current maximum height from left side (that is from A[0,1...a])      and the maximum height from right side(from A[b,b+1...n-1])      if(leftmax < rightmax) then, at least (leftmax-A[a]) water can definitely be stored          no matter what happens between [a,b]          since we know there is a barrier at the rightside(rightmax>leftmax).           On the other side, we cannot store more water than (leftmax-A[a]) at index a          since the barrier at left is of height leftmax.           So, we know the water that can be stored at index a is exactly (leftmax-A[a]).       The same logic applies to the case when (leftmax>rightmax).  At each loop we can make a and b one step closer

--- Leetcode/042. trapping Rain Water.java
+++ Leetcode/042. trapping Rain Water.java
@@ -7,33 +7,50 @@
 输出: 6
 
 
-一个位置一个位置算, 
-一个下标(位置) 能盛下的雨水量 = 它左右两边柱子的最大高度(left_max, right_max)的最小值 - 它的高度     
+一列一列的算, 
+一个是从左更新 一个是从右更新, 相向双指针
 
-所以就变成了 如何找所有位置的左右两边柱子的最大高度 left_max, right_max
+一个下标(位置) 能盛下的雨水量 = 它左右两边柱子的最大高度(left_max, right_max)的较小值 - 它的高度     
 
-但是 一个是从左更新 一个是从右更新
-所以用到双指针, 然后 用 if (height[left] <= height[right]) 判断左边还是右边
-                再用 一个 if (height[left] < left_max) 或者 if (height[right] < right_max) 来判断是不是可以盛水
+
+
+ I calculated the stored water at each index a and b in my code. 
+ At the start of every loop, 
+    I update the current maximum height from left side (that is from A[0,1...a]) 
+    and the maximum height from right side(from A[b,b+1...n-1])
+
+    if(leftmax < rightmax) then, at least (leftmax-A[a]) water can definitely be stored 
+        no matter what happens between [a,b] 
+        since we know there is a barrier at the rightside(rightmax>leftmax). 
+
+        On the other side, we cannot store more water than (leftmax-A[a]) at index a 
+        since the barrier at left is of height leftmax. 
+
+        So, we know the water that can be stored at index a is exactly (leftmax-A[a]). 
+
+    The same logic applies to the case when (leftmax>rightmax).
+
+At each loop we can make a and b one step closer
+
 
 
 
 class Solution {
-    public int trap(int[] height) {
-        if (height == null || height.length == 0) return 0;
-        int left = 0, right = height.length - 1;
-        int left_max = 0;
-        int right_max = 0;          
+    public int trap(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int left = 0, right = nums.length - 1;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
         int ans = 0;
-        while (left < right) {              
-            if (height[left] <= height[right]) {         //左边比右边小,  当前处理的就是左边这个下标
-                                                        //那么要是更新两边柱子的最大高度(left_max, right_max)的最小值, 就是会更新在左边
-                if (height[left] < left_max)    ans += left_max - height[left];
-                else left_max = height[left];
+        while (left <= right) {
+            leftMax = Math.max(leftMax, nums[left]);
+            rightMax = Math.max(rightMax, nums[right]);
+            
+            if (leftMax < rightMax) {
+                ans += leftMax - nums[left];
                 left++;
             } else {
-                if (height[right] < right_max)  ans += right_max - height[right];
-                else right_max = height[right];
+                ans += rightMax - nums[right];
                 right--;
             }
         }
@@ -41,10 +58,11 @@ class Solution {
     }
 }
 
+
 O(n) O(1)
 
 
-// https://leetcode-cn.com/problems/trapping-rain-water/solution/zuo-you-liang-bian-de-zui-da-zhi-by-powcai/
+
 
 
 
