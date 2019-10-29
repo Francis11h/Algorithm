7e0aa55e101b2009b7bb6078cba1e8e309b5f9a2
Francis <42663441+Francis11h@users.noreply.github.com>
2019-10-29 15:09:52 -0400

    Update Graph.java

--- tips/Graph.java
+++ tips/Graph.java
@@ -7,8 +7,8 @@ adjacency list
 edge list	(用于MST kruskal)
 
  
-								adjacency matrix		adjacency list
-inspect all edges					O(V^2)					O(E)
+					adjacency matrix		adjacency list
+inspect all edges			O(V^2)					O(E)
 Find all edges incident on i 		O(V)					O(k)
 Is there an edge from i to j		O(1)					O(logk)
 
