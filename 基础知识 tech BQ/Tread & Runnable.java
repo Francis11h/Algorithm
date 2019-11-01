438ad7c570284ad505bfcc081996cd215a3aacaf
Francis <42663441+Francis11h@users.noreply.github.com>
2019-11-01 11:48:07 -0400

    Update Tread & Runnable.java

--- 基础知识 tech BQ/Tread & Runnable.java
+++ 基础知识 tech BQ/Tread & Runnable.java
@@ -22,6 +22,27 @@ class Thread1 extends Thread {
     }
 }
 
+
+方式一：
+    自定义一个类继承Thread.
+    子类重写run方法，把自定义线程的任务定义在run方法上。
+    创建thread子类的对象，并且调用start方法开启线程。
+方式二：
+    自定义一个类去实现Runnable接口。
+    实现了Runnable接口的run方法， 把自定义线程的任务定义在run方法上。
+    创建Runnable实现类的对象。
+    创建Thread对象，并且把Runnable实现类对象作为参数传递进去。
+    调用thread对象的start方法开启线程。
+    
+疑问1： Runnable实现类对象是线程对象吗？
+    runnable实现类的对象并不是一个线程对象，只不过是实现了Runnable接口的对象而已。
+疑问2： 为什么要把Runnable实现类的对象作为参数传递给thread对象呢？作用是什么？
+    作用： 是把Runnable实现类的对象的run方法作为了任务代码去执行了。
+
+
+
+
+
 class Thread2 implements Runnable {
     private String name;
 
