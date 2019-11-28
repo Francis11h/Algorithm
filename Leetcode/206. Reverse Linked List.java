02c3f9ffbe568b72cc33fa69e833274d63391431
Francis11h <zhan23@ncsu.edu>
2019-11-28 10:35:40 -0500

    重要的是 如何用递归实现 reverse, 三种方式

--- Leetcode/206. Reverse Linked List.java
+++ Leetcode/206. Reverse Linked List.java
@@ -20,21 +20,31 @@ p   c
                         p    c
 
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
-        ListNode prev = null, current = head;
-        while (current != null) {
-            ListNode Temp = current.next;
-            current.next = prev;
-            prev = current;
-            current = Temp;
+        
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
         }
         return prev;
     }
 }
 
-每次换的时候 用个 临时 Node 记录下 current节点原来next指向的 Node, 否则会丢失
+每次换的时候 用个 临时 Node 记录下 cur节点原来next指向的 Node, 否则会丢失
 
 O(n)
 O(1)
@@ -42,12 +52,82 @@ O(1)
 
 
 //recursive
+
+
+
+
+2019.11.28 reverse  a linkedlist recursively
+
+Start with node cur as head
+
+// boundary cases
+1. if the cur is null, return null    
+2. if the cur.next is null, this means it is the last node, 
+  so make this as "head" beacuse the last node will be the head of reversed list.
+
+3. Recursively traverse the list
+// reverse  the node pointers
+4. set cur.next.next to  cur
+5. set cur.next to null
+
+
+what should the recursion return? --->  the head of the reverseList 
+
+
+
 class Solution {
-  public ListNode reverseList (ListNode head) {
-    if (head == null || head.next == null) return head;
-    ListNode p = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return p;
+  public ListNode reverseList(ListNode head) {
+      return reverseListRecurisvely(head);
   }
+  // return the head of the reversedlinkedlist 
+  public ListNode reverseListRecurisvely(ListNode cur) {
+      // boundary case1 empty list
+      if (cur == null) return null;
+      // boundary case2, cur is the last node, which should be the head of the reversed list, so we. should return cur
+      if (cur.next == null) return cur;
+      // then we will do the reverse recurisvely
+      ListNode newHead = reverseListRecurisvely(cur.next);
+      cur.next.next = cur;
+      cur.next = null;
+      return newHead;
+  }
+}
+
+
+第二种写法
+
+class Solution {
+  ListNode newHead;
+  public ListNode reverseList(ListNode head) {
+      reverseListRecurisvely(head);
+      return newHead;
+  }
+  // return the head of the reversedlinkedlist 
+  public void reverseListRecurisvely(ListNode cur) {
+      // boundary case1 empty list
+      if (cur == null) return;
+      // boundary case2, cur is the last node, which should be the head of the reversed list, so we. should return cur
+      if (cur.next == null) {
+        newHead = cur;
+        return;
+      }
+      // then we will do the reverse recurisvely
+      reverseListRecurisvely(cur.next);
+      cur.next.next = cur;
+      cur.next = null;
+  }
+}
+
+第三种 完全模拟 iterate, 即传两个 node 一个代表现在 一个代表之前
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+    
+    private ListNode reverse(ListNode cur, ListNode prev) {
+        if (cur == null) return prev;
+        ListNode next = cur.next;       // save current's next
+        cur.next = prev;                // point cur.next to prev
+        return reverse(next, cur);      // forward current node to next, set the cur node as prev
+    }
 }
