0baa14b265c249565399aea1b531fd213d6dc8d7
Francis11h <zhan23@ncsu.edu>
2019-11-30 19:50:19 -0500

    右边指针先动 直到重复 左边再动 去重

--- Leetcode/003. 最长无重复子串 Longest Substring Without Repeating Characters .java
+++ Leetcode/003. 最长无重复子串 Longest Substring Without Repeating Characters .java
@@ -1,5 +1,7 @@
 3. 最长无重复字符子串 Longest Substring Without Repeating Characters
 
+Given a string, find the length of the longest substring without repeating characters.
+
 Input: "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3. 
@@ -60,3 +62,82 @@ class Solution {
 
 T : O(N)
 S : O(26)  
+
+
+
+2019.11.30 重新写 一遍过的版本
+// we can use one pass solution, we need two pointers one moves first until the substring has a repeated character, 
+// then move another pointer to eliminate the duplicate, during this process record the length of the substrings without repeating characters
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        // initialize two pointers 
+        int left = 0, right = 0;
+        //use a map to record the # of each char
+        Map<Character, Integer> map = new HashMap<>();
+        int ans = 0;
+        while (right < s.length()) {
+            // count the new char 
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            
+            // if occur a duplicate begin to remove
+            while (map.get(ch) > 1) {
+                char rmv = s.charAt(left);
+                map.put(rmv, map.get(rmv) - 1);
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+}
+
+
+
+follow up: instead of returning count, return the longest substring itself
+
+
+记录起点坐标 
+// if we not only want the length, but we also want the substrings itself, 
+// we can add a set to record the begin indexes of the substrings
+
+class Solution {
+    public List<String> lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int left = 0, right = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        // add a set to record  the begin indexes of the longest substrings
+        Set<Integer> set = new HashSet<>();
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            
+            while (map.get(ch) > 1) {
+                char rmv = s.charAt(left);
+                map.put(rmv, map.get(rmv) - 1);
+                left++;
+            }
+            // record or clear when we have a longer length
+            if (right - left + 1 > max) {
+                set.clear();
+                max = right - left + 1;
+                set.add(left);
+            } else if (right - left + 1 == max) {
+                set.add(left);
+            }
+            right++;
+        }
+        
+        Set<String> ans = new HashSet<>();
+        for (Integer i : set) {
+            System.out.println(s.substring(i, i + max));
+            ans.add(s.substring(i, i + max));
+        }
+        return new ArrayList<>(ans);
+    }
+}
+
