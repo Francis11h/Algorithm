9ac6317495c3f4fd79e1929f94b97ff4daa330f9
Francis11h <zhan23@ncsu.edu>
2019-12-02 13:56:22 -0500

    转为 求 depth 途中更新diameter   非递归方法用 postorder （stack + map）

--- Leetcode/543. Diameter of Binary Tree.java
+++ Leetcode/543. Diameter of Binary Tree.java
@@ -18,6 +18,8 @@ This path may or may Not pass through the root.
 =>  维护一个全局变量 diameter 每次dfs 判断是否需要修改其即可
 
 
+The longest path that passes a given node as the ROOT node is T = left_height+right_height. 
+So you just calculate T for all nodes and output the max T.
 
 // convert to calculate the depth of the leaves 
 // cause the longest path must be one leaf to another leaf
@@ -44,3 +46,61 @@ class Solution {
 
 time complexity : O(N) we visted every node once.
 space complexity : the size of our implicit call stack during our dfs
+
+
+
+
+
+
+
+
+
+Solution 2: Iterative Method
+
+The idea is to use PostOrder traversal which to 
+                    ensure the availability of the node 
+                              "until its left and right subtrees are processed".
+
+For this reason, we use peek() method to keep the node on the stack 
+                        until its left and right subtrees gets processed.
+
+Then for each node, find the maximum-depth of the left and right subtrees. 
+                  Using this maximum-depth, we update diameter if required.
+
+
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null ) return 0;
+        
+        Map<TreeNode, Integer> map = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        int diameter = 0;
+        
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            
+            if (node.left != null && !map.containsKey(node.left)) {
+                stack.push(node.left);
+            } else if (node.right != null && !map.containsKey(node.right)) {
+                stack.push(node.right);
+            } else {
+                stack.pop();
+                int leftDepth = map.getOrDefault(node.left, 0);
+                int rightDepth = map.getOrDefault(node.right, 0);
+                // record node's depth
+                map.put(node, Math.max(leftDepth, rightDepth) + 1);
+                // update the diameter
+                diameter = Math.max(diameter, leftDepth + rightDepth);
+            }
+        }
+        return diameter;
+    }
+}
+
+
+
+Time complexity : O(n) - since every node in the tree is visited
+Space complexity : O(n) - space required for the HashMap and Stack
