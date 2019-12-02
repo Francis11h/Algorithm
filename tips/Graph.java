e6a1f9efcc937ba6a34402e7fa0957f10f612e87
Francis <42663441+Francis11h@users.noreply.github.com>
2019-12-02 14:34:14 -0500

    instance

--- tips/Graph.java
+++ tips/Graph.java
@@ -13,3 +13,20 @@ Find all edges incident on i 		O(V)					O(k)
 Is there an edge from i to j		O(1)					O(logk)
 
 
+
+临街表建图 实例 List[], 之后 每个index 都要new出来 list 对象
+
+	List[] graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] pre : prerequisites) {
+            int from = pre[1], to = pre[0];
+            // add edge for our directed graph
+            // there are no duplicate edges in the input prerequisites.
+            graph[from].add(to);
+            indegree[to]++;
+        }
