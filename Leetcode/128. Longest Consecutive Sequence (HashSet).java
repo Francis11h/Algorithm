e3a3dbc685220752f7bf4a7de0baea8e0640f82a
Francis11h <zhan23@ncsu.edu>
2019-12-04 00:53:44 -0500

    直接 排序 找 也可以

--- Leetcode/128. Longest Consecutive Sequence (HashSet).java
+++ Leetcode/128. Longest Consecutive Sequence (HashSet).java
@@ -43,12 +43,42 @@ class Solution {
     }
 }
 
-
+O(n)
 "set.remove() 返回的是 boolean"
 
 
 
+Solution 2: 更朴素的想法 排序即可
 
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int longestStreak = 1;      
+        int currentStreak = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {     // remove depuliacte
+                if (nums[i] == nums[i-1]+1) {
+                    currentStreak += 1;
+                }
+                else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
+            }
+        }
+
+        return Math.max(longestStreak, currentStreak);
+    }
+}
+
+
+O(nlogn)
 
 
 
