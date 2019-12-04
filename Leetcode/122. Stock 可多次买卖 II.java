ccf0fb1a51c01ee4f44ddafaf7d609286a89cb81
Francis11h <zhan23@ncsu.edu>
2019-12-03 22:40:59 -0500

    dp也可以做

--- Leetcode/122. Stock 可多次买卖 II.java
+++ Leetcode/122. Stock 可多次买卖 II.java
@@ -48,6 +48,65 @@ In case we skip one of the peaks 如果我们跳过其中的一个peek, we will 
 
 
 
+Solution2 dp解法1
+
+dp[i] 表示 第i天结束 最大利润
+dp[i] = Math.max(dp[i - 1], prices[i] - (prices[j] - dp[j - 1]));
+    1. if we dont trade, then the profit is same as previous day "dp[i-1]"
+    2. if we bought stock j-th day, where j=[0...i-1], then sell the stock on i-th day,
+        then the profit is ("prices[i] - prices[j] + dp[j - 1]").
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int[] dp = new int[prices.length];          
+
+        for (int i = 1; i < prices.length; i++) {
+            int min = prices[0];
+            for (int j = 1; j <= i; j++) {                      //所有i之前的天 都可能买, 计算最小的买入价格, 同时减去之前 白嫖赚的
+                min = Math.min(min, prices[j] - dp[j - 1]);
+            }
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
+        }
+
+        return dp[prices.length - 1];
+    }
+}
+
+
+        [7,1,5, 3, 6, 4]
+min      7 1 1 -1 -1 -1
+dp[i]    0 0 4  4  7  7 
+
+Solution2 dp解法2 优化 因为 min运算 一直在重复
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int[] dp = new int[prices.length];          
+
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            // 每次 新来一个 price 都计算出 买入价 - 之前几个trans 白嫖的利润
+            min = Math.min(min, prices[i] - dp[i - 1]);
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
+        }
+
+        return dp[prices.length - 1];
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -65,7 +124,7 @@ Follow Up: 可以同时持有多支股票 但是一天只能进行一次买or卖
     直到 数组中 再也找不到利润
 
     [1,1,5,9,3,20] test case 失败
-
+    ... 感觉是 np问题
 
     
 
