6ad75f983b8181bf4d05e7e27b82c71a16dd420b
Francis11h <zhan23@ncsu.edu>
2020-01-31 16:29:09 -0500

    Set<Integer> col = new HashSet<>();     Set<Integer> sum = new HashSet<>();     Set<Integer> diff = new HashSet<>();

--- Leetcode/052. N-Queens II .java
+++ Leetcode/052. N-Queens II .java
@@ -3,6 +3,72 @@
 改为求个数了 更简单
 就不需要 board[][] 了 直接 抽想着求即可
 
+
+
+
+
+2020.1.31
+
+class Solution {
+    char[][] board;
+    Set<Integer> col = new HashSet<>();
+    Set<Integer> sum = new HashSet<>();
+    Set<Integer> diff = new HashSet<>();
+    int ans = 0;
+    public int totalNQueens(int n) {
+        if (n <= 0) return 0;
+        //build the chessboard
+        board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        //search
+        dfs(0, n);
+        return ans;
+    }
+
+    private void dfs(int row, int n) {
+        if (row == n) {
+            ans++;
+            return;
+        }
+
+        //each column
+        for (int i = 0; i < n; i++) {
+            if (!col.contains(i) && !sum.contains(i + row) && !diff.contains(i - row)) {
+                board[row][i] = 'Q';
+                col.add(i);
+                sum.add(i + row);
+                diff.add(i - row);
+
+                dfs(row + 1, n);
+                // backtracking
+                board[row][i] = '.';
+                col.remove(i);
+                sum.remove(i + row);
+                diff.remove(i - row);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     
     int ans = 0;
