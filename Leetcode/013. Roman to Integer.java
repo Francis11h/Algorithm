7696a0a72195d3c73a20595d952382c6df192150
Francis11h <zhan23@ncsu.edu>
2020-02-06 15:21:29 -0500

    挨个遍历字符串 当前位比后面小就减去这位 否则就加

--- Leetcode/013. Roman to Integer.java
+++ Leetcode/013. Roman to Integer.java
@@ -45,3 +45,21 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+不建hash表也可以 就 用一个switch函数
+
+private int getValue(char ch) {
+    switch(ch) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
