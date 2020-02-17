81a4521d45260c283ceee4121da27998cddd8afe
Francis11h <zhan23@ncsu.edu>
2020-02-17 01:44:07 -0500

    PrintWriter pw = new PrintWriter("output\\" + fileName + ".out", "UTF-8");

--- Java源码系列/常用函数/读文件.java
+++ Java源码系列/常用函数/读文件.java
@@ -28,3 +28,35 @@ private static void getInputFromFile(String fileName) throws FileNotFoundExcepti
         e.printStackTrace();
     }
 }
+
+
+
+PrintWriter pw = new PrintWriter("output\\" + fileName + ".out", "UTF-8");
+pw.println();
+pw.print();
+
+pw.close();
+
+
+
+
+
+private static void WriteIntoFile(String fileName) throws FileNotFoundException {
+    try {
+        Output = new ArrayList<>();
+
+        System.out.println("\n------output " + fileName);
+        PrintWriter pw = new PrintWriter("output\\" + fileName + ".out", "UTF-8");
+
+        pw.println(Output.size());
+        System.out.println(Output.size());
+
+        for (int i = 0; i < Output.size(); i++) {
+            pw.print(Output.get(i) + " ");
+            System.out.println(Output.get(i) + " ");
+        }
+        pw.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
