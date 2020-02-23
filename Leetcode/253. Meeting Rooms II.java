c03fb87e7d036221ffa5493157f553f616822253
Francis11h <zhan23@ncsu.edu>
2020-02-22 20:41:10 -0500

    建个"小根堆 minHeap", 存结束时间,方面我们每次找可以最先空出来的

--- Leetcode/253. Meeting Rooms II.java
+++ Leetcode/253. Meeting Rooms II.java
@@ -15,16 +15,16 @@ Output: 1
 用的room最少
 所以我们要把之前开完会的room分配给之后需要开会的
 所以我们需要知道会议开始时间的先后顺序,------> sort by starting time
-第二步就是考虑如何分配了 :  策略就两个 没meeting room available => allocate a new one '/' 有room has freed up(释放),不用新加
+第二步就是考虑如何分配了 :  策略就两个 没 meeting room available => allocate a new one '/' 有room has freed up(释放),不用新加
 
-我们 不care 哪个room gets freed up 仅仅是有就行
+我们 不care 哪个 room gets freed up 仅仅是有就行
 
 我们知道现在已经分配的room, 同时知道这些room的解冻时间
 We already know the rooms we have allocated till now and we also know when are they due to get free because of the end times of the meetings going on in those rooms. 
 我们只需要找到所有被分配的room中 结束时间最早的, 把该room分给新来的 meeting 就可以。
 We can simply check the room which is due to get vacated the earliest amongst all the allocated rooms.
 
-所以我们可以建个小根堆, 存结束时间,方面我们每次找可以最先空出来的
+所以我们可以建个"小根堆 minHeap", 存结束时间,方面我们每次找可以最先空出来的
 要是老room释放的时间 < 新meeting开始的 , 老的poll
 每次 新加新的 meeting 的 endtime
 堆中元素的个数 就是我们需要的meeting room的数量
@@ -70,32 +70,31 @@ class Solution {
 
 
 
+建个"小根堆 minHeap", 存结束时间,方面我们每次找可以最先空出来的
+
 其实 最后的 heap.size() 就是 会议室 个数
 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) return 0;
+        if (intervals == null || intervals.length == 0 || intervals[0].length == 0) return 0;
         Arrays.sort(intervals, new Comparator<int[]>(){
-            @Override
-            public int compare (int[] a, int[] b) {
+           @Override
+            public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
-
+        
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        minHeap.offer(intervals[0][1]);
-
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= minHeap.peek()) {
+        
+        for (int[] m : intervals) {
+            if (!minHeap.isEmpty() && minHeap.peek() <= m[0]) {
                 minHeap.poll();
-            }
-            minHeap.offer(intervals[i][1]);
+            } 
+            minHeap.offer(m[1]);
         }
         return minHeap.size();
     }
 }
-
 
 
 
